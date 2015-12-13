62a6c5781bd185411517342a1faf825079f01441
Viranch Mehta <viranch.mehta@gmail.com>
2015-12-13 18:54:28 +0530

    Implement UI for book an appointment section

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
     $('[data-toggle="tab"]').tooltip()
+
+    $('#accordion .panel-heading').click(function() {
+        $(this).parent().find('.collapse').collapse('toggle');
+    });
+
+    // collapse all other collapsibles when a collapsible is shown
+    $('.collapse').on('show.bs.collapse', function() {
+        var showing = $(this).attr('id');
+
+        $('.collapse').each(function() {
+            //if (showing != $(this).attr('id')) {
+                $(this).collapse('hide');
+            //}
+        });
+    });
 });
+
+function makeid() {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+function captchaCallback() {
+    $('.g-recaptcha').each(function() {
+        $(this).attr('id', makeid());
+        grecaptcha.render($(this).attr('id'), {'sitekey': '6Lcn8BITAAAAAM-OFAx5x1GoZLT__SCoLM8_ZmoQ'});
+    });
+}
